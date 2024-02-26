1707d829d01af61144f04a3ffd7b4c0bb2eddbcc
dawnbreaker <74147543+dawnbreaker28@users.noreply.github.com>
2024-02-26 02:14:04 -0500

    Add files via upload

---
+++ around/constants/constant.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_USERNAME = "dawnbreaker"
+	ES_PASSWORD = "ws20010207ws"
+	GCS_BUCKET  = "around202309"
+)
---
+++ around/model/model.go
@@ -0,0 +1,19 @@
+package model
+
+type Post struct {
+	Id      string `json:"id"`
+	User    string `json:"user"`
+	Message string `json:"message"`
+	Url     string `json:"url"`
+	Type    string `json:"type"`
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Age      int64  `json:"age"`
+	Gender   string `json:"gender"`
+}
+
+// user dawnbreaker
+// password ws20010207ws
